c333d31c24ba807f049f180ba2fa0f2f46c3bc62
ityme <ityme@outlook.com>
2025-12-27 23:50:54 +0800

    修改复制逻辑: 批量复制所有主题入口文件 (i-w-*.css)

--- setup.bat
+++ setup.bat
@@ -4,19 +4,20 @@ setlocal
 
 :: 源路径
 set "file1=iWonder"
-set "file2=i-w-blue.css"
+set "file2=i-w-*.css"
 set "file3=conf.user.json"
 
 :: 目标路径
 set "DST=%APPDATA%\Typora"
 
-
-
-
 :: 复制 主题样式文件夹
 xcopy "%cd%\%file1%\*" "%DST%\themes\%file1%" /e /i /y
-:: 复制 主题入口文件
-copy  "%cd%\%file2%" "%DST%\themes"
+
+:: 批量复制所有主题入口文件 (i-w-*.css)
+for %%F in ("%cd%\%file2%") do (
+    copy "%%F" "%DST%\themes"
+)
+
 :: 复制 快捷键配置文件
 copy  "%cd%\%file3%" "%DST%\conf"
 
